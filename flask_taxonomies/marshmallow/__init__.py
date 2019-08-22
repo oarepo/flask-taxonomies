5b8808292062fc0dadb6a99635593bf8f5530e57
Mirek Simek <miroslav.simek@vscht.cz>
2019-08-22 15:12:19 +0200

    isort

--- flask_taxonomies/marshmallow/__init__.py
+++ flask_taxonomies/marshmallow/__init__.py
@@ -3,7 +3,7 @@
 from invenio_records_rest.schemas import StrictKeysMixin
 from invenio_records_rest.schemas.fields import SanitizedUnicode
 from marshmallow import ValidationError, pre_load
-from marshmallow.fields import Nested, Integer
+from marshmallow.fields import Integer, Nested
 from sqlalchemy.orm.exc import NoResultFound
 
 from flask_taxonomies.models import Taxonomy
