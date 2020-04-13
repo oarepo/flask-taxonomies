f0258f627576374fc553696ed097b969a63c3dc8
Mirek Simek <miroslav.simek@vscht.cz>
2020-04-13 07:48:22 +0200

    removed sources

--- flask_taxonomies/config.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Configuration for Flask Taxonomies."""
-
-FLASK_TAXONOMIES_PERMISSION_FACTORY = \
-    'flask_taxonomies.permissions.permission_factory'
-"""Permission factory to control the REST access to Taxonomy objects."""
