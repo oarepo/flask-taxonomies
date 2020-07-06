68be4b436b30f0b807f70f16046c57da1122dfa8
Mirek Simek <miroslav.simek@vscht.cz>
2020-07-06 19:53:21 +0200

    reordering source code

--- flask_taxonomies/fields.py
+++ flask_taxonomies/fields.py
@@ -19,40 +19,6 @@ class Descendant(ColumnElement):
         self.rhs = rhs
 
 
-@compiles(Descendant)
-def compile_descendant(element, compiler, **kw):
-    expr = Grouping(element.lhs.op('like')(element.rhs + ESCAPE_CHAR + '%').op('or')(element.lhs.op('=')(element.rhs)))
-    return compiler.visit_grouping(expr)
-
-
-@compiles(Descendant, 'postgresql')
-def compile_descendant(element, compiler, **kw):
-    lhs = element.lhs
-    rhs = element.rhs
-    if isinstance(rhs, str):
-        rhs = rhs.replace('/', '.').replace('-', '_')
-    expr = Grouping(lhs.op('<@')(rhs))
-    return compiler.visit_grouping(expr)
-
-
-@compiles(Ancestor)
-def compile_ancestor(element, compiler, **kw):
-    expr = Grouping(element.lhs.op('||')(ESCAPE_CHAR + '%').reverse_op('like')(element.rhs).op('or')(
-        element.lhs.op('=')(element.rhs)
-    ))
-    return compiler.visit_grouping(expr)
-
-
-@compiles(Ancestor, 'postgresql')
-def compile_ancestor(element, compiler, **kw):
-    lhs = element.lhs
-    rhs = element.rhs
-    if isinstance(rhs, str):
-        rhs = rhs.replace('/', '.').replace('-', '_')
-    expr = Grouping(lhs.op('@>')(rhs))
-    return compiler.visit_grouping(expr)
-
-
 # postgresql ltree does not allow for hyphens in path, so need to change them to _
 class PostgresSlugType(LtreeType):
     class comparator_factory(types.Concatenable.Comparator):
@@ -119,6 +85,40 @@ class SlugType(types.TypeDecorator):
     def process_result_value(self, value, dialect):
         if value is not None:
             return value.replace(ESCAPE_CHAR, '/')
+
+
+@compiles(Descendant)
+def compile_descendant(element, compiler, **kw):
+    expr = Grouping(element.lhs.op('like')(element.rhs + ESCAPE_CHAR + '%').op('or')(element.lhs.op('=')(element.rhs)))
+    return compiler.visit_grouping(expr)
+
+
+@compiles(Descendant, 'postgresql')
+def compile_descendant(element, compiler, **kw):
+    lhs = element.lhs
+    rhs = element.rhs
+    if isinstance(rhs, str):
+        rhs = rhs.replace('/', '.').replace('-', '_')
+    expr = Grouping(lhs.op('<@')(rhs))
+    return compiler.visit_grouping(expr)
+
+
+@compiles(Ancestor)
+def compile_ancestor(element, compiler, **kw):
+    expr = Grouping(element.lhs.op('||')(ESCAPE_CHAR + '%').reverse_op('like')(element.rhs).op('or')(
+        element.lhs.op('=')(element.rhs)
+    ))
+    return compiler.visit_grouping(expr)
+
+
+@compiles(Ancestor, 'postgresql')
+def compile_ancestor(element, compiler, **kw):
+    lhs = element.lhs
+    rhs = element.rhs
+    if isinstance(rhs, str):
+        rhs = rhs.replace('/', '.').replace('-', '_')
+    expr = Grouping(lhs.op('@>')(rhs))
+    return compiler.visit_grouping(expr)
 
 
 @compiles(sa.UnicodeText, 'postgresql')
