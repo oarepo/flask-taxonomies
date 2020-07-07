91d0b67e410665a6cac4111d550864952db66cd3
Mirek Simek <miroslav.simek@gmail.com>
2020-07-07 07:48:48 +0200

    7.0.0 alpha release (#78)
    Co-authored-by: Mirek Simek <miroslav.simek@vscht.cz>

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ tests_require = [
 
 setup(
     name='flask-taxonomies',
-    version='7.0.0.dev1',
+    version='7.0.0a1',
     packages=['flask_taxonomies', ],
     install_requires=requires,
     tests_require=tests_require,
