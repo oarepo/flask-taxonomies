c79fa9a3754a708cc651fd4e8dbd4d296e91861d
Mirek Simek <miroslav.simek@gmail.com>
2020-07-12 12:17:23 +0200

    Alpha 2 release

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ tests_require = [
 
 setup(
     name='flask-taxonomies',
-    version='7.0.0a1',
+    version='7.0.0a2',
     packages=['flask_taxonomies', ],
     install_requires=requires,
     tests_require=tests_require,
