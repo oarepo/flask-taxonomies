294f650d5f61da17d40e29f32a12a2a8424de871
Mirek Simek <miroslav.simek@gmail.com>
2020-07-14 22:09:00 +0200

    a5 version due to mistake in release branch

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ tests_require = [
 
 setup(
     name='flask-taxonomies',
-    version='7.0.0a4',
+    version='7.0.0a5',
     packages=['flask_taxonomies', ],
     install_requires=requires,
     tests_require=tests_require,
